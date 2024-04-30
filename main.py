48cf3629e219a41c6bacc7fe0387ef23033b9c20
Jagmohan Chauhan <jagmohan.barkot@gmail.com>
2024-05-01 00:45:34 +0530

    data ingestion notebook added

---
+++ main.py
@@ -0,0 +1,13 @@
+from textsummarizernlp.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
+from textsummarizernlp.logging import logger
+
+STAGE_NAME = 'Data Ingestion Stage'
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed<<<<<\n\nx=======x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+    
